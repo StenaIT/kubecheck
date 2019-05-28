64e28291eea9afe2bab3fe935b39ca775a063f5d
Kristoffer Ahl <git@77dynamite.com>
2019-05-28 13:05:06 +0200

    - Added a hook package for handling webhooks.

---
+++ hook/webhook.go
@@ -0,0 +1,44 @@
+package hook
+
+import (
+	"bytes"
+
+	"github.com/StenaIT/kubecheck/http"
+	"github.com/apex/log"
+)
+
+// Event defines an event
+type Event string
+
+// Webhook defines a webhook
+type Webhook struct {
+	Name   string
+	URI    string
+	Data   string
+	Events []Event
+}
+
+// TriggerWebhooks invokes webhooks matching the event
+func TriggerWebhooks(hooks []Webhook, e Event) {
+	if hooks != nil {
+		for _, hook := range hooks {
+			if contains(hook.Events, e) {
+				log.WithFields(log.Fields{
+					"hook":  hook.Name,
+					"event": e,
+				}).Info("Invoking webhook")
+				body := bytes.NewReader([]byte(hook.Data))
+				http.NewClient(hook.URI).Post("", body)
+			}
+		}
+	}
+}
+
+func contains(s []Event, e Event) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
